2b6f0fe3ed15729462b6bc3d753efca613ded1da
Nikita Voloboev <nikita.voloboev@gmail.com>
2019-06-16 22:04:23 +0100

    update

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Test } from "./components/Test";
 
-// TODO: add routing & have /test always be routed so I can experiment on it
+function Test() {
+  return <div />;
+}
+
 const App = () => <Test />;
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- react/src/test.js
+++
@@ -1,7 +0,0 @@
-function sum(a) {
-  return function(b) {
-    return a + b; // takes "a" from the outer lexical environment
-  };
-}
-
-console.log(sum);
