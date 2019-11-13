3714fd3a1db2b50019853c39f045969e1c11d56f
Nikita Voloboev <nikita.voloboev@gmail.com>
2019-11-13 16:32:04 +0000

    update

--- bash/test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-subl ~/Dropbox/Write/knowledge/looking-back/2019/2019-"$(date +%B | tr '[:upper:]' '[:lower:]')".md
--- python/test.py
+++
@@ -1,9 +0,0 @@
-# from requests import get
-import requests
-from bs4 import BeautifulSoup
-from lxml import html
-
-page = requests.get("https://app.otta.co/companies")
-print(page)
-tree = html.fromstring(page.content)
-print(tree)
